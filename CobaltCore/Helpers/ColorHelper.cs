08f0fffcbe59c68f338b50ab5c721a3d2a4a4b7d
Kronox <dev@kronox.me>
2021-08-26 12:08:39 +0200

    added advanced ui tooling

---
+++ CobaltCore/Helpers/ColorHelper.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace CobaltCore.Helpers
+{
+    public class ColorHelper
+    {
+        public static uint AsRgb(Color color)
+        {
+            return ((color.packedValue & 0x000000FF) << 16) | (color.packedValue & 0x0000FF00) | ((color.packedValue & 0x00FF0000) >> 16);
+        }
+    }
+}
---
+++ CobaltCore/Helpers/CombatTextHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using TShockAPI;
+
+namespace CobaltCore.Helpers
+{
+    public class CombatTextHelper
+    {
+        public static void CreateCombatText(TSPlayer player, string message, uint color = 4294967295)
+        {
+            CreateCombatText(player, message, new Vector2(player.X, player.Y), color);
+        }
+
+        public static void CreateCombatText(TSPlayer player, string message, Vector2 pos, uint color = 4294967295)
+        {
+            player.SendData(PacketTypes.CreateCombatTextExtended, message, (int) color, pos.X, pos.Y);
+        }
+    }
+}
