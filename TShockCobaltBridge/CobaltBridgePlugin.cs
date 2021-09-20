6fa83c0d693c45299c75a15c5f542166ecae7166
Kronox <dev@kronox.me>
2021-09-20 18:02:01 +0200

    added basic tshock bridge skeleton

---
+++ TShockCobaltBridge/CobaltBridgePlugin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Terraria;
+using TerrariaApi.Server;
+
+namespace TShockCobaltBridge
+{
+    [ApiVersion(2, 1)]
+    public class CobaltBridgePlugin : TerrariaPlugin
+    {
+        public override string Author => "Kronox";
+        public override string Description => "Cobalt Base Plugin";
+        public override string Name => "Cobalt";
+        public override Version Version => new Version(1, 0, 0, 0);
+        
+        public CobaltBridgePlugin(Main game) : base(game)
+        {
+        }
+
+        public override void Initialize()
+        {
+            
+        }
+    }
+}
